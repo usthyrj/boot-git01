3bddf5566ff0b73c671b5035db857910a30dfdf9
regis_global <regis75@foxmail.com>
2020-03-23 14:05:17 +0800

    第一次提交

---
+++ src/main/java/ltd/leshan/bootgit/controller/HelloController.java
@@ -0,0 +1,19 @@
+package ltd.leshan.bootgit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: HelloController
+ * @Description: TODO
+ * @author: 闫荣君
+ * @date: 2020/3/23  13:23
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/info")
+    public String info(){
+        return "Hello World";
+    }
+}
